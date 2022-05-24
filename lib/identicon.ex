063b96192800a42a5b2cba8419ae209f8e163ccc
Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
2022-05-24 15:03:34 -0300

    add method `hash_input`

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,16 +3,22 @@ defmodule Identicon do
   Documentation for `Identicon`.
   """
 
+  def main(input) do
+    input
+    |> hash_input
+  end
+
   @doc """
-  Hello world.
+  Generate a list of bytes, based in a given string.
 
   ## Examples
 
-      iex> Identicon.hello()
-      :world
-
+      iex> Identicon.hash_input("banana")
+      [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
   """
-  def hello do
-    :world
+  @spec hash_input(String.t()) :: list(byte())
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
   end
 end
