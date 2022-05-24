e9ffba3130c5e3e3496b03fc7dfa01542b91f277
Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
2022-05-24 16:18:53 -0300

    update method hash_input

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -10,15 +10,11 @@ defmodule Identicon do
 
   @doc """
   Generate a list of bytes, based in a given string.
-
-  ## Examples
-
-      iex> Identicon.hash_input("banana")
-      [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
   """
-  @spec hash_input(String.t()) :: list(byte())
   def hash_input(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
   end
 end
