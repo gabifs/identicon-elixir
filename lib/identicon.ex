73fc636b4cf82c5dd6f21a88b54386f0f2545732
Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
2022-05-24 21:07:41 -0300

    add method build_grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -7,6 +7,26 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
+  end
+
+  def build_grid(image) do
+    %Identicon.Image{hex: hex} = image
+
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+
+    row ++ [second, first]
   end
 
   def pick_color(image) do
